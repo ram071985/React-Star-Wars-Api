0e3558c0c4f72e8f1d52518e8d60788425efe4ab
ram071985 <reidmuchow@gmail.com>
2020-03-10 08:58:45 -0500

    adeded readme and screen shot of app

--- src/LoadingSpinner.js
+++ src/LoadingSpinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Container } from "react-bootstrap";
 
  const LoadingSpinner = () => (
-  <Container>
+
       <i className="fa fa-spinner fa-spin" /> Loading...
-    </Container>
+  
 
   )
 
